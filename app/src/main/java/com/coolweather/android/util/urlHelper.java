193f258a1dff694bdac6ede167f3638d53f1a451
Lwq <695487971@qq.com>
2017-10-12 18:07:57 +0800

    完成数据库初始化

---
+++ app/src/main/java/com/coolweather/android/util/urlHelper.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.util;
+
+/**
+ * Created by Adminisitrator on 2017/10/12.
+ */
+
+public class urlHelper {
+    public static final String address_url="http://guolin.tech/api/china";
+
+    public static final String heWeather_url="https://free-api.heweather.com/v5/weather?";
+
+    public static final String heWeatherKey="c065cbd314954c1f9aaaa5d7f2d77bd9";
+}
