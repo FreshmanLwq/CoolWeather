4ef602d07360b37823b16db0c8d3f5dc18ba6413
Lwq <695487971@qq.com>
2017-11-20 16:01:21 +0800

    加入显示天气信息和获取必应每日一图的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Adminisitrator on 2017/11/18.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
--- app/src/main/java/com/coolweather/android/util/urlHelper.java
+++ app/src/main/java/com/coolweather/android/util/urlHelper.java
@@ -7,7 +7,9 @@ package com.coolweather.android.util;
 public class UrlHelper {
     public static final String address_url="http://guolin.tech/api/china";
 
-    public static final String heWeather_url="https://free-api.heweather.com/v5/weather?";
+    public static final String heWeather_url="https://free-api.heweather.com/v5/weather?city=";
 
-    public static final String heWeatherKey="c065cbd314954c1f9aaaa5d7f2d77bd9";
+    public static final String heWeatherKey="&key=c065cbd314954c1f9aaaa5d7f2d77bd9";
+
+    public static final String bing_pic = "http://guolin.tech/api/bing_pic";
 }
